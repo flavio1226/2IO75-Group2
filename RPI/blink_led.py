b88688083748ce2f7abc83d7d87d793b627ca1e3
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2022-05-19 20:07:02 +0200

    flashing led and motor driver classes are now done.

---
+++ RPI/blink_led.py
@@ -0,0 +1,22 @@
+# @author Group 2
+# @created at 02.05.2022
+# @purpose make a simple LED blink
+
+# importing the needed RPi library
+import RPi.GPIO as GPIO
+from time import sleep # needed for the function sleep
+
+# initial setup for the raspberry pi pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(18, GPIO.OUT) # select port 18 for the shorter leg of the LED
+
+# repeat endlessly with delays of one second
+while True:
+
+    print("LED is now flashing....")
+    GPIO.output(18, GPIO.HIGH) # make the LED on port 18 flash at its highest brightness
+    sleep(1) # delay the program by 1 second
+    print("LED is now off...")
+    GPIO.output(18, GPIO.LOW) # make the LED on port 18 stop flashing.
---
+++ RPI/motor_driver.py
@@ -0,0 +1,11 @@
+# @author Group 2
+# @created at 02.05.2022
+# @purpose connect and work with the L293D arduino motor controller
+
+#importing the needed AMSpi library for controlling the motors
+from AMSpi import AMSpi
+
+with AMSpi() as amspi:
+    amspi.set_74HC595_pins(21, 20, 16) # set the pins of the 74HC595 motor shield
+    asmpi.set_L293D_pins(5, 6, 13, 19) # set the pins of the L293D motor shield
+    asmpi.run_dc_motors(asmpi.DC_Motor_1, asmpi.DC_Motor_2, asmpi.DC_Motor_3, asmpi.DC_Motor_4) # make the motors on ports M1, M2, M3, M4 start running at full speed
