f621cfc6cb2149180b68912e5bb84a015e2cc25d
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2022-05-22 11:00:20 +0200

    testing motors

--- INO/dc_motor.ino
+++ INO/dc_motor.ino
@@ -9,7 +9,8 @@
 // include the needed library --> AdaFruit Motor
 #include <AFMotor.h>
 
-AF_DCMotor motor(1);
+AF_DCMotor conveyorMotor(1); // motor situated on the port M1 on the motor controller L293D
+AF_DCMotor beltMotor(2); // motor situated on the port M2 on the motor controller L293D
 
 /**
  * @brief setup function
@@ -18,8 +19,11 @@ AF_DCMotor motor(1);
  */
 void setup() {
     // set initial dcmotor speed to 200 and direction RELEASE
-    motor.setSpeed(200);
-    motor.run(RELEASE);
+    conveyorMotor.setSpeed(200);
+    conveyorMotor.run(RELEASE);
+
+    beltMotor.setSpeed(200);
+    beltMotor.run(RELEASE);
 }
 
 /**
@@ -28,14 +32,24 @@ void setup() {
  * @details this function runs continuously untill the end of the program.
  */
 void loop() {
-    // stop motor
-    motor.setSpeed(0);
-    // make the motor run BACKWARD
-    motor.run(BACKWARD);
-    // delay for 5000 ms == 5 s
-    delay(5000);
-    // make the motor run FORWARD
-    motor.run(FORWARD);
-    // set the new power
-    motor.setSpeed(200);
+    // // stop motor
+    // motor.setSpeed(0);
+    // // make the motor run BACKWARD
+    // motor.run(BACKWARD);
+    // // delay for 5000 ms == 5 s
+    // delay(5000);
+    // // make the motor run FORWARD
+    // motor.run(FORWARD);
+    // // set the new power
+    // motor.setSpeed(200);
+
+    motorTest();
+}
+
+void motorTest() {
+  beltMotor.run(FORWARD);
+  delay(1000);  // run forward for 1 second
+  beltMotor.run(RELEASE);
+  delay(100);  // 'coast' for 1/10 second
+  beltMotor.run(BACKWARDS);  // run in reverse
 }
